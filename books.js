408eeb1863d87b616587a650dbe4c35cdf28d79a
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-18 21:15:44 +0100

    J'ai termine la function ajouter et  la function de deletetask

--- books.js
+++ books.js
@@ -26,14 +26,32 @@ let books = [
 ];
 
 
+let addBookbtn=document.querySelector('#btnAddBook')
+let count= 4
 
+displayBooks()
 // Display books on the page
 function displayBooks() {
     const bookList = document.getElementById('bookList');
-    bookList.innerHTML = '<h1>gfgfgfgfgfhfg</h1>'; // Clear the book list
+
+    bookList.innerHTML =  ``// Clear the book list
     for (let i = 0; i < books.length; i++) {
+      const bookElement = document.createElement("li");
+      bookElement.className =
+        "list-group-item d-flex justify-content-between align-items-center";
         
-        
+        bookElement.innerHTML += `
+           <span>
+                ${books[i].title}  |  <strong> ${books[i].author}</strong> 
+            </span>
+            <div>
+                <button class="btn btn-${books[i].status === "read" ? "secondary" : "success"} btn-sm me-2" onclick="toggleStatus(${books[i].id})">
+                    ${ books[i].status === "read"? "not read": "read"}
+                </button>
+                <button class="btn btn-danger btn-sm" onclick="deleteBook(${books[i].id})">Delete</button>
+            </div>
+        `
+        bookList.appendChild(bookElement);
     }
 
    
@@ -43,8 +61,24 @@ function displayBooks() {
 function addBook() {
     const titleInput = document.getElementById('new-book-title');
     const authorInput = document.getElementById('new-book-author');
+    const spanError=document.getElementById('error')
+     count ++
+     newBook={
+      "id": count,
+        "title":  titleInput.value,
+        "author": authorInput.value,
+        "status": "not read"
 
-    
+     }
+     if (titleInput.value == '' &&titleInput.value == '' ) {
+      spanError.innerHTML=` Veuillez remplire les samps`
+      spanError.style.color= "red";
+      
+      
+     }else{
+      spanError.innerHTML=``
+       books.push(newBook)}
+   
     
 
 
@@ -63,8 +97,15 @@ function addBook() {
 
 // Delete a book from the list
 function deleteBook(id) {
-   
+ let index = books.findIndex(e=>e.id ===id)
+  
+  
+   books.splice(index, 1);
     
+
+   //Do not touch these
+   saveBooks();
+   displayBooks();
 }
 
 // Save books to local storage
@@ -80,3 +121,17 @@ window.onload = () => {
         displayBooks();
     }
 };
+
+function toggleStatus(id) {
+   let finStatud
+  status === "read"? "not read": "read"
+}
+
+
+
+
+
+
+// appel du function addBoks
+
+addBookbtn.addEventListener('click',addBook)
